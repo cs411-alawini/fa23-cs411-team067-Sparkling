d8a8872e43d773bc04ff5b6a615cf14d36469cf9
clmiao-illinois <clmiao@illinois.edu>
2023-12-05 22:35:03 -0600

    [revise] us-flight-delay-map

---
+++ flight-delay/us-flight-delay-map/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <FlightDelayMap />
+  </div>
+</template>
+
+<script>
+import FlightDelayMap from './components/FlightDelayMap.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FlightDelayMap,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
